1917d66d08039cf0efda81937d04795153de12bc
RomanLatipov <romanlatipov@gmail.com>
2024-06-29 20:44:57 -0400

    complete roman-numerals-to-integer lab

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,22 @@
 function romanNumeral(string) {
-  // type your code here
+  const nums = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  let total = 0;
+
+  for (let i=0; i<string.length; i++) {
+    if (nums[string[i]] < nums[string[i+1]])
+      total -= nums[string[i]];
+    else
+      total += nums[string[i]];
+  }
+  return total;
 }
 
 if (require.main === module) {
